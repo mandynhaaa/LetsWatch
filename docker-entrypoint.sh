1743f6724c35dc79a175d820b23f2391b2330a99
mandynhaaa <amandatobler18@gmail.com>
2026-01-07 17:15:10 +0000

    refactor: force database fresh migration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,10 @@
-#!/bin/bash
 set -e
 
-echo "🚀 Iniciando setup de produção..."
-
-# Otimizações do Laravel
-echo "🔥 Otimizando cache..."
 php artisan config:cache
 php artisan event:cache
 php artisan route:cache
 php artisan view:cache
 
-# Rodar migrações (Force é necessário em produção)
-echo "🐘 Rodando migrações do banco..."
-php artisan migrate --force
+php artisan migrate:fresh --force
 
-# Iniciar Apache em foreground
-echo "🎬 Iniciando servidor..."
 apache2-foreground
