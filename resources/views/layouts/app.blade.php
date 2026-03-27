00504f65318ec126c27ce01eebe86613dcbac7f3
mandynhaaa <amandatobler18@gmail.com>
2026-03-27 18:26:19 -0300

    fix: add copy.js to vite.config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="images/logoicon.png">
     <title>@yield('title', 'Let\'s Watch')</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js','resources/js/cards.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/cards.js'])
     @stack('scripts')
 </head>
 <body class="min-h-screen bg-[var(--bg)] text-[var(--text)]">
